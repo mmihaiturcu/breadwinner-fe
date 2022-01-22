8ddab576e27596184d776215935fd4857b44b575
Mihai Turcu <mihai.turcu@cctech.ai>
2022-01-22 23:40:42 +0200

    refactor - added scaling effect to data cloud animation

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -581,12 +581,15 @@ $default-logo-size: 200px;
 
 @keyframes pulse {
     0% {
+        transform: scale(1);
         box-shadow: 0 0 0 0 rgba(0, 120, 67, 0.5);
     }
     70% {
+        transform: scale(0.9);
         box-shadow: 0 0 0 10px rgba(0, 120, 67, 0);
     }
     100% {
+        transform: scale(1);
         box-shadow: 0 0 0 0 rgba(0, 120, 67, 0);
     }
 }
